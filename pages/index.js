e739ce43a94c3f6d0cbc0ab0c2ad4a60d0565e21
pradip.vadher <pradip@frontendarmy.com>
2023-05-11 16:32:59 +0530

    edited

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js by Pradip Vadher Edited 04:24!</a>
+          Welcome to <a href="https://nextjs.org">Next.js by Pradip Vadher Edited 04:24! 04:32</a>
         </h1>
 
         <p className={styles.description}>
